b65ced96cf5bda4c7fd4c28de7560c6f9816ccea
951549510@adm.unifor.br <guilhermesaraivax88@gmail.com>
2022-05-26 13:31:16 -0300

    include auth controller manager login

--- src/main/java/br/userauthjwt/authjwt/controller/authApi.java
+++ src/main/java/br/userauthjwt/authjwt/controller/authApi.java
@@ -1,5 +1,23 @@
 package br.userauthjwt.authjwt.controller;
 
+import br.userauthjwt.authjwt.config.authRequest;
+import br.userauthjwt.authjwt.config.authResponse;
+import br.userauthjwt.authjwt.misc.managerAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/auth/")
 public class authApi {
+
+    @Autowired
+    private managerAuthentication manager;
+
+    @PostMapping("login")
+    @ResponseStatus(HttpStatus.OK)
+    public authResponse signIn(@RequestBody authRequest req){
+        return this.manager.authUser(req);
+    }
+
 }
